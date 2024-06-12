a73d4c38a2b1c18f8f71d6ae1c20f10d2b8d4a4b
Svilen Ivanov <134698218+Svilkata88@users.noreply.github.com>
2024-06-12 23:55:41 +0300

    5 to 14th problem solved for this ex

---
+++ 6.2.subqueries_and_joins_ex/14.minimum_average_area_across_continents.sql
@@ -0,0 +1,8 @@
+SELECT
+    MIN(average_area) AS min_average_area
+FROM (
+    SELECT
+        AVG(area_in_sq_km) AS average_area
+    FROM countries AS c
+    GROUP BY continent_code
+) AS average_area;
---
+++ 6.2.subqueries_and_joins_ex/8.hahns_bookings.sql
@@ -0,0 +1,6 @@
+SELECT
+    count(c.last_name)
+FROM bookings AS b JOIN
+    customers AS c
+    USING (customer_id)
+WHERE c.last_name = 'Hahn'
